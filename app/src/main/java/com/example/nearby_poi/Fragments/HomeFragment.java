ea26b8f0154560d53202a277c7f2e16df62f7022
jain <gautam.34595@tmu.ac.in>
2023-07-13 19:07:43 +0530

    User Location Update Done

--- app/src/main/java/com/example/nearby_poi/Fragments/HomeFragment.java
+++ app/src/main/java/com/example/nearby_poi/Fragments/HomeFragment.java
@@ -1,9 +1,13 @@
 package com.example.nearby_poi.Fragments;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.res.ResourcesCompat;
 import androidx.fragment.app.Fragment;
 
@@ -15,14 +19,32 @@ import com.example.nearby_poi.Constants;
 import com.example.nearby_poi.PlacesModel;
 import com.example.nearby_poi.R;
 import com.example.nearby_poi.databinding.FragmentHomeBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.chip.Chip;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
 
-public class HomeFragment extends Fragment implements OnMapReadyCallback{
+public class HomeFragment extends Fragment implements OnMapReadyCallback {
     FragmentHomeBinding binding;
     private GoogleMap map;
+    private FusedLocationProviderClient client;
+    SupportMapFragment mapFragment;
+
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -34,7 +56,8 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.homeMap);
+        client= LocationServices.getFusedLocationProviderClient(getContext());
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.homeMap);
         mapFragment.getMapAsync((OnMapReadyCallback) this);
         for (PlacesModel placesModel : Constants.placeName) {
 
@@ -45,6 +68,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
             chip.setTextColor(getResources().getColor(R.color.black, null));
             chip.setChipIcon(ResourcesCompat.getDrawable(getResources(), placesModel.getDrawableId(), null));
             chip.setCheckable(true);
+           // chip.setBackgroundColor(getResources().getColor(R.color.black, null));
             chip.setCheckedIconVisible(false);
 
             binding.chipGroup.addView(chip);
@@ -54,6 +78,66 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback{
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
-        map= googleMap;
+        map = googleMap;
+        Dexter.withContext(getContext().getApplicationContext())
+                .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        setUpGMaps();
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                }).check();
     }
+
+    private void setUpGMaps() {
+
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        map.setMyLocationEnabled(true);
+        map.getUiSettings().setTiltGesturesEnabled(true);
+        Task<Location> task = client.getLastLocation();
+
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                mapFragment.getMapAsync(new OnMapReadyCallback() {
+                    @Override
+                    public void onMapReady(@NonNull GoogleMap googleMap) {
+                        LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                        MarkerOptions markerOptions = new MarkerOptions().position(latLng)
+                                .title("Your Location")
+                                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+                        map.addMarker(markerOptions);
+                        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,17));
+                    }
+                });
+            }
+        });
+
+    }
+
+    private void stopLocation(){
+
+    }
+
+
+
 }
